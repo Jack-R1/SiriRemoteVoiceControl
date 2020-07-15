f3c3621ad952ef2ce5ccce77efab85ea43e415e7
JackR1 <JackR1>
2020-07-15 21:22:33 +1000

    Initial commit.

---
+++ SiriRemoteVoiceControl/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SiriRemoteVoiceControl
+//
+//  Created by Jack on 15/7/20.
+//  Copyright © 2020 JackR1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
